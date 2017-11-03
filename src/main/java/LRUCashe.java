76f2e9293334cff688805d723cc4b57cf53b2d25
antondolgiy <antondolgiy@yandex.ru>
2017-11-03 17:08:19 +0300

    added "reverse linked list"

--- src/main/java/LRUCashe.java
+++ src/main/java/LRUCashe.java
@@ -5,110 +5,110 @@ import java.util.HashMap;
 /**
  * Created by Anton on 24.10.2017.
  */
-public class LRUCashe<K,V> {
+public class LRUCashe<K, V> {
 
-    HashMap<K,Node> map=new HashMap();
-
-
+    HashMap<K, Node> map = new HashMap();
     int capacity;
     //first=old,first candidate to b deleted; last=fresh,"just used"
     Node first;
     Node last;
-    class Node{
+
+    class Node {
         Node previous;
         K key;
         V value;
         Node next;
 
-        public Node(Node previous,K key,V value,Node next ){
-            this.previous=previous;
-            this.key=key;
-            this.value=value;
-            this.next=next;
-
+        public Node(Node previous, K key, V value, Node next) {
+            this.previous = previous;
+            this.key = key;
+            this.value = value;
+            this.next = next;
         }
+
         @Override
-        public String toString(){
-            return "NODE(key:"+key+","+"value:"+value+")";
+        public String toString() {
+            return "NODE(key:" + key + "," + "value:" + value + ")";
         }
-
-
-
-
     }
-    LRUCashe(int capacity){
-        if(capacity<3){
+
+    public LRUCashe(int capacity) {
+        if (capacity < 3) {
             System.out.println("make it at least 3 dude...");
             throw new IllegalArgumentException();
         }
-        this.capacity=capacity;
+        this.capacity = capacity;
     }
+
     public void putNod(K key, V value) {
         if (map.size() == 0) {
             Node nod = new Node(null, key, value, null);
             map.put(key, nod);
             first = nod;
             last = nod;
-        }
-
-        else {
+        } else {
 
             if (map.containsKey(key)) {
                 Node refreshed = map.get(key);
-                if(refreshed==last){
+                if (refreshed == last) {
                     // should cashed value be replaced with new value? if yeas then
-                    refreshed.value=value;
+                    refreshed.value = value;
 
-                    ;}
-                else if (refreshed==first){
-                    // should cashed value be replaced with new value? if yeas then
+                    ;
+                } else if (refreshed == first) {
+                    /*
                     refreshed.value=value;
+                    first = refreshed.next;
+                    first.previous = null;
+                    refreshed.next=null;
+                    refreshed.previous = last;
+                    last=refreshed;
+                    */
+                    // should cashed value be replaced with new value? if yeas then
+                    refreshed.value = value;
 
                     //make link from current last element to new last elment
-                    last.next=first;
+                    last.next = first;
                     //make link from new to carrent last element
-                    first.previous=last;
+                    first.previous = last;
                     //find what element will be first
-                    Node tobe1st=first.next;
+                    Node tobe1st = first.next;
                     //as future first element it should have previous
-                    tobe1st.previous=null;
+                    tobe1st.previous = null;
                     //and future last element should not have next
-                    first.next=null;
+                    first.next = null;
                     //declare first as last
-                    last=first;
+                    last = first;
                     //and declare tobe1st as first
 
-                    first=tobe1st;
-                }
-                else{
+                    first = tobe1st;
+                } else {
                     // should cashed value be replaced with new value? if yeas then
-                    refreshed.value=value;
+                    refreshed.value = value;
 
-                    refreshed.previous.next=refreshed.next;
-                    refreshed.next.previous=refreshed.previous;
+                    refreshed.previous.next = refreshed.next;
+                    refreshed.next.previous = refreshed.previous;
 
                     refreshed.previous = last;
                     refreshed.next = null;
                     //вот этой одной строчкой ссылку забыл перекинуть -  и жЁпа
-                    last.next=refreshed;
-                    last=refreshed;
+                    last.next = refreshed;
+                    last = refreshed;
                 }
-            }
-            else{
-                if (map.size()<capacity){
+            } else {
+                if (map.size() < capacity) {
 
                     Node nod = new Node(last, key, value, null);
                     last.next = nod;
                     last = nod;
                     map.put(key, nod);
 
-                }
-                else {
+                } else {
 
                     map.remove(first.key);
-                    Node toBeFirst=first.next;
-                    toBeFirst.previous=null;
-                    first=toBeFirst;
+                    Node toBeFirst = first.next;
+                    toBeFirst.previous = null;
+                    first = toBeFirst;
                     Node nod = new Node(last, key, value, null);
                     last.next = nod;
                     last = nod;
@@ -119,48 +119,48 @@ public class LRUCashe<K,V> {
 
         }
     }
-    Node getNod(K key){
-        if(map.containsKey(key)){
-        Node refreshed = map.get(key);
-        if(refreshed==last){
-            ;}
-        else if (refreshed==first){
 
-           //make link from current last element to new last elment
-            last.next=first;
-           //make link from new to carrent last element
-            first.previous=last;
-            //find what element will be first
-            Node tobe1st=first.next;
-            //as future first element it should have previous
-            tobe1st.previous=null;
-            //and future last element should not have next
-            first.next=null;
-            //declare first as last
-            last=first;
-            //and declare tobe1st as first
+    Node getNod(K key) {
+        if (map.containsKey(key)) {
+            Node refreshed = map.get(key);
+            if (refreshed == last) {
+                ;
+            } else if (refreshed == first) {
 
-            first=tobe1st;}
+                //make link from current last element to new last elment
+                last.next = first;
+                //make link from new to carrent last element
+                first.previous = last;
+                //find what element will be first
+                Node tobe1st = first.next;
+                //as future first element it should have previous
+                tobe1st.previous = null;
+                //and future last element should not have next
+                first.next = null;
+                //declare first as last
+                last = first;
+                //and declare tobe1st as first
 
+                first = tobe1st;
+            } else {
+                refreshed.previous.next = refreshed.next;
+                refreshed.next.previous = refreshed.previous;
 
-        else{
-            refreshed.previous.next=refreshed.next;
-            refreshed.next.previous=refreshed.previous;
+                refreshed.previous = last;
+                refreshed.next = null;
+                //вот этой одной строчкой ссылку забыл перекинуть -  и жЁпа
+                last.next = refreshed;
+                last = refreshed;
+            }
 
-            refreshed.previous = last;
-            refreshed.next = null;
-            //вот этой одной строчкой ссылку забыл перекинуть -  и жЁпа
-            last.next=refreshed;
-            last=refreshed;
+            return refreshed;
+        } else {
+            System.out.println("no sach key");
+            return null;
         }
 
-        return refreshed;}
-
-    else {  System.out.println("no sach key");
-            return null;}
-
     }
 
 
-    }
+}
 
---
+++ src/main/java/SpecialList.java
@@ -0,0 +1,153 @@
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Anton on 03.11.2017.
+ */
+
+ public class SpecialList<T> {
+        int size=0;
+        private Node head=null;
+        private Node tail=null;
+
+        class Node{
+            private T element;
+            Node next=null;
+            Node previous=null;
+            public Node(T element){
+                this.element=element;
+            }
+
+        }
+
+        public T removeLast(){
+            T element=null;
+            if(size==0){
+                throw new NoSuchElementException();
+            }
+            if(size==1){
+                element= tail.element;
+                head=null;
+                tail=null;
+                size--;
+            }
+            else {element= tail.element;
+                tail=tail.previous;
+                tail.next=null;
+                size--;
+            }
+            return element;
+
+
+        }
+
+        public void addElement(T element){
+
+            Node node = new Node(element);
+            if (size == 0) {
+                head = node;
+                tail = node;
+                node.next = null;
+                node.previous = null;
+                size++;
+
+            } else {
+                tail.next = node;
+                node.previous = tail;
+                tail = node;
+                size++;
+            }
+        }
+
+        public static SpecialList reverseList(SpecialList list){
+            SpecialList list1 =new SpecialList();
+            while (list.size>0){
+                list1.addElement(list.removeLast());
+            }
+
+            return list1;
+        }
+
+        private   void printAllNodeElement(Node node){
+            if(node.previous==null) System.out.println(node.element);
+            if(node.next!=null){
+                System.out.println(node.next.element);
+                printAllNodeElement(node.next);
+            }
+
+
+
+        }
+
+        private   void SwapAllNodeElement(Node node){
+
+            if(node.next!=null){
+                Node shadow=node.previous;
+                if(shadow==null){
+
+                    tail=node;}
+                else {
+                    if(shadow!=null){
+                        Node temp=null;
+                        temp=shadow.next;
+                        shadow.next=shadow.previous;
+                        shadow.previous=temp;
+                    }
+                }
+                SwapAllNodeElement(node.next);
+            }
+            else {
+                Node shadow=node.previous;
+                Node temp=null;
+                temp=shadow.next;
+                shadow.next=shadow.previous;
+                shadow.previous=temp;
+                node.next=shadow;
+                node.previous=null;
+                head=node;
+
+            }
+        }
+
+        public void printAllvalues(){
+            this.printAllNodeElement(this.head);
+        }
+        public void swapAll(){
+            this.SwapAllNodeElement(this.head);
+
+        }
+
+
+
+
+
+
+
+
+
+        public static void main(String[] args) {
+            SpecialList<String> list=new SpecialList<String>();
+            list.addElement("aa");
+            list.addElement("bb");
+            list.addElement("cc");
+            list.addElement("dd");
+
+            System.out.println(list.size);
+            list.printAllvalues();
+            System.out.println("************");
+
+            list.swapAll();
+
+            System.out.println("************");
+            System.out.println(list.size);
+            list.printAllvalues();
+
+
+
+
+        }
+
+
+
+    }
+
+
